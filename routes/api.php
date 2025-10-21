41ad6b48600545d5f02a19a93e4699254c633955
Tanzid Haque <tanzid3@gmail.com>
2025-10-22 01:26:22 +0600

    Test API ROute

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json([
+        'name' => 'Borka API Collection',
+        'version' => '1.0.0',
+        'country' => "Bangladesh",
+        "current_time" => now()->format('Y-m-d H:i:s'),
+    ], 200);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
